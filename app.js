9f335ca5b20a4ecf99e8e93c335976a5d93fe626
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-28 20:49:30 +0530

    fixed cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://127.0.0.1:5500", credentials: true }));
 
 //Routers
 const userRouter = require("./router/userRouter");
